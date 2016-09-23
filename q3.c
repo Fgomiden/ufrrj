11d0b67b2ade8137168a73b786d57fa48344c7fb
Caio Sabadin <caiosabadin@users.noreply.github.com>
2016-09-23 13:51:26 -0300

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,97 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(void) {
-   
-    float valorTotal;
-    int qtdAcessorios;
-   
-    qtdAcessorios = 0;
- 
-    mostraModelos(&valorTotal);
-   
-    while (mostraAcessorios(&qtdAcessorios,&valorTotal)){
-       
-    }
- 
-   
-//  printf("\nValor sem desconto: %f",valorTotal);
-   
-    calculaDescontos(&valorTotal,&qtdAcessorios);
-   
-    printf("\nvalor total a pagar: %f",valorTotal);
-   
-    printf("\nqtdAcessorios: %d",qtdAcessorios);
-   
- 
+
+typedef struct dadosCompra {
+	
+	int modeloCarro;
+	int qtdAcessorios;
+	int valorAcessorios;
+	float valorTotal;
+	
+} dadosCompra;
+
+
+int main (void){
+	
+	float total;
+	
+	dadosCompra *dCompra = (dadosCompra *)malloc(sizeof(dadosCompra));
+	
+	dCompra->valorAcessorios = 0;
+	
+	selecionaModelo(dCompra);
+	
+	selecionaAcessorios(dCompra);
+	
+
+		if (dCompra->qtdAcessorios == 2){
+			
+			dCompra->valorTotal = dCompra->valorTotal + (dCompra->valorAcessorios - dCompra->valorAcessorios*0.1);
+		}
+		
+		if (dCompra->qtdAcessorios == 3){
+			
+			dCompra->valorTotal = dCompra->valorTotal + (dCompra->valorAcessorios - dCompra->valorAcessorios*0.15);
+		}
+		
+		if (dCompra->qtdAcessorios == 5){
+			
+			dCompra->valorTotal = dCompra->valorTotal + (dCompra->valorAcessorios - dCompra->valorAcessorios*0.2);
+		}
+	
+	
+	printf("\nValor total a pagar: %f", dCompra->valorTotal);
+	
+	return 0;
+	
 }
- 
- 
-calculaDescontos(float *pValorTotal,int *qtdAcessorios){
-   
-   
-    float valor;
-    valor = *pValorTotal;
-   
-   
-    switch (*qtdAcessorios){
-       
-        case 2:
-            *pValorTotal = valor-valor*0.1;
-           
-            break;
-           
-        case 3:
-            *pValorTotal = valor-valor*0.15;
-            break;         
-        case 5:
-            *pValorTotal = valor-valor*0.2;
-            break;
-       
-    }  
-}
- 
- 
- mostraModelos(float *pValorTotal){
-    int modeloSelecionado;
-   
+
+
+selecionaModelo(dadosCompra *cpr){
+	
+	int modeloSelecionado;
+	
     printf("Selecione o modelo:\n\n");
     printf("1 - Dib_Turbo $20.000\n");
     printf("2 - Dib_MV7 $15.000\n");
     printf("3 - Dib_Stander $12.000\n");
-           
-    scanf("%d",&modeloSelecionado);
+    
+    scanf("%d", &modeloSelecionado);
+    
+	cpr->modeloCarro = modeloSelecionado;
+	
+    
+    switch (modeloSelecionado){
+    	
+    	case 1:
+    		cpr->valorTotal = 20000;
+    		break;
+    	case 2:
+    		cpr->valorTotal = 15000;
+    		break;
+    	case 3:
+    		cpr->valorTotal = 12000;
+    		break;
+    		
+    	default:
+    		break;
+   	}
        
-    switch (modeloSelecionado)
-   {
-   
-        case 1 :
-            *pValorTotal = 20000;
-            break;
-       
-        case 2 :
-            *pValorTotal = 15000;
-            break;
-           
-        case 3 :
-            *pValorTotal = 12000;
-            break;
-       
-        default :
-            printf("valor invalido");
-              break;   
-    }  
-   
-   
- 
 }
- 
-int mostraAcessorios(int *qtdAcessorios, float *valorTotal){
-   
-    int opcionalSelecionado = 0;
-   
-   
-    printf("Selecione os opcionais:\n\n");
+
+
+selecionaAcessorios(dadosCompra *cpr){
+	
+	int acessorioSelecionado = 0;
+	
+	while (acessorioSelecionado < 6){
+		
+	printf("Selecione os opcionais:\n\n");
     printf("1 - Ar condicionado $2.000\n");
     printf("2 - Dir. hidraulica $1.500\n");
     printf("3 - Motor 2.0 $1.000\n");
@@ -99,49 +94,42 @@ int mostraAcessorios(int *qtdAcessorios, float *valorTotal){
     printf("5 - Bancos de couro $1.000\n");
     printf("6 - Sair\n");
    
-    scanf("%d",&opcionalSelecionado);
-   
-    switch (opcionalSelecionado){
-       
-        case 1:
-            *valorTotal += 2000;
-            *qtdAcessorios+=1;
-            return 1;
-            break;
-           
-        case 2:
-            *valorTotal += 1500;
-            *qtdAcessorios+=1;
-            return 1;
-            break;
-           
-        case 3:
-            *valorTotal += 1000;
-            *qtdAcessorios+=1;
-            return 1;
-            break;
-           
-        case 4:
-            *valorTotal += 1000;
-            *qtdAcessorios+=1;
-            return 1;
-            break;
-           
-        case 5:
-            *valorTotal += 1000;
-            *qtdAcessorios+=1;
-            return 1;
-            break;
-           
-        case 6:
-            return 0;
-            break;
-           
-        default :
- 
-            break;
-       
-    }
- 
-   
+    scanf("%d",&acessorioSelecionado);
+    
+    
+    
+    switch (acessorioSelecionado){
+    	
+    	case 1:
+    		
+    		cpr->valorAcessorios += 2000;
+    		cpr->qtdAcessorios += 1;
+      		break;
+    		
+   		 case 2:
+    		cpr->valorAcessorios += 1500;
+    		cpr->qtdAcessorios += 1;
+    		break;
+   		 case 3:
+    		cpr->valorAcessorios += 1000;
+    		cpr->qtdAcessorios += 1;
+    		break;
+    	case 4:
+    		cpr->valorAcessorios += 1000;
+    		cpr->qtdAcessorios += 1;
+    		break;
+    	case 5:
+    		cpr->valorAcessorios += 1000;
+    		cpr->qtdAcessorios += 1;
+    		break;
+    	case 6:
+    		break;
+    		
+    	default:
+    		cpr->valorAcessorios = 0;
+    		break;
+   	
+		}
+         
+	}
 }
